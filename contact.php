8197845189c1a84bc5c3f5be38b40a579ef97bb5
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-08-10 11:04:27 +1200

    creating contact page, site becomes small bug

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php include("topbit.php") ?>
+
+        <div class="box main">
+
+        <h2>Contact Us</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <iframe class="contact" src="https://docs.google.com/forms/d/e/1FAIpQLScGd_DxpeLn3F0DkDjsINTdhRfpwrqaDJ0Y4-si3AU-9EaInA/viewform?embedded=true" height="686">Loading…</iframe>
+
+        </div>
+
+<?php include("bottombit.php") ?>
