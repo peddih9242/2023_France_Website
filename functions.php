e71dba04d1d1a336369e235bf889a520ea03bb30
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-08-14 15:00:20 +1200

    updated function so gallery can work

--- functions.php
+++ functions.php
@@ -1,41 +1,20 @@
 <?php
 
 // function displays all images in a folder as a gallery while taking descriptions for each
-function make_gallery($folder_name, $descriptions) {
+function make_gallery($folder_name, $descriptions, $titles) {
             
     $dirname = "images/$folder_name/";
     $images = glob($dirname."*.jpg");
     $count = 0;
 
     foreach($images as $images) {
-        
-        // get image properties
-        $exif = exif_read_data($images, 0, true);
-
-        foreach ($exif as $key => $section) {
-            foreach ($section as $name => $val) {
-
-                if($key == 'IFD0') {
-                    
-                    // strips 'special' characters, allows a-z,
-                    // spaces, dashes and round brackets
-                    $val = preg_replace('/[^A-Za-z0-9\- ()]/', '', $val);
-                
-                    $title = $val;
-
-                }
-
-            } // end $section loop
-
-        } // end $exif loop
 
 ?>
 
     <div class="responsive-gallery">
 
         <div class="lightbox gallery">
-
-        <a href="<?php echo $images ?>" class="big"><img src="<?php echo $images ?>" alt=<?php echo $title ?> title=<?php echo $title ?>></a>
+        <a href="<?php echo $images ?>" class="big"><img src="<?php echo $images ?>" alt="<?php echo $titles[$count] ?>" title="<?php echo $titles[$count] ?>"></a>
             <p class="desc"><?php echo $descriptions[$count] ?></p>
         </div>
 
