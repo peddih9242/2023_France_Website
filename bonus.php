092a3b0f7525a3266bcebc6da523f286d6f7c510
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-08-16 12:24:47 +1200

    adding bonus page for after contact form is complete

---
+++ bonus.php
@@ -0,0 +1,16 @@
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/layout.css">
+
+    <div class="box main">
+
+        <h2>Bonus Page</h2>
+
+        <img src="images/aurelien.jpg" style="width: 50%;" alt="Aurelien" title="Aurelien">
+        
+        <p>Meet Aurelien, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia orci vitae iaculis vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vitae consequat turpis. Vivamus et nibh est. In semper neque sed lacus placerat tincidunt eu et nisi. Praesent placerat vehicula turpis vitae tincidunt. Maecenas magna nibh, tristique vel euismod condimentum, malesuada non nibh.</p>
+        
+    </div>
